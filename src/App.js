73879a6daf5bf77d202c87185f196a547d5e13df
yucheng <yuchenghsu@gmail.com>
2024-03-31 01:37:19 +0800

    feat: add ageGroupSelect component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import AgeGroupSelect from './components/AgeGroupSelect/AgeGroupSelect';
 import PriceInput from './components/PriceInput/PriceInput';
 
 function App() {
   const [value, setValue] = useState('0');
+  const [ageGroup, setAgeGroup] = useState([7, 17]);
   return (
     <div>
       <PriceInput onChange={(e) => setValue(e.target.value)} value={value} />
+      <AgeGroupSelect isOverlap={false} ageGroup={ageGroup} setAgeGroup={(e) => setAgeGroup(e)} />
     </div>
   );
 }
